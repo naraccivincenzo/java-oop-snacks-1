2037e81b5c97256e37321b22815353e1792684c6
naracci.vincenzo <143946955+naraccivincenzo@users.noreply.github.com>
2024-07-26 23:13:43 +0200

    Snack File Completed

---
+++ src-snack-file/org/lessons/java/snack/file/Snack_file.java
@@ -0,0 +1,18 @@
+package org.lessons.java.snack.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Snack_file {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\Flage\\Desktop\\Java\\java-oop-snacks-1\\src-snack-file\\test.txt");
+
+        try (Scanner scanner = new Scanner(file)) {
+            System.out.println(scanner.nextLine());
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
